ce30f5fbc075e5fae32cb5bcfa0c8848ac719f07
LEOFIST <54279651+LEOFIST@users.noreply.github.com>
2022-08-28 20:25:21 -0500

    Add files via upload

---
+++ take-home-webdriver-test/scr/JavaTest/test_Script1.js
@@ -0,0 +1,20 @@
+// JavaScript test-script code
+import com.browserstack.local.Local;
+// Creates an instance of Local
+Local bsLocal = new Local();
+  
+// You can also set an environment variable - "BROWSERSTACK_ACCESS_KEY".
+HashMap<String, String> bsLocalArgs = new HashMap<String, String>();
+  
+bsLocalArgs.put("key", "sGbibVQzigMskey8i8xL");
+  
+// Starts the Local instance with the required arguments
+bsLocal.start(bsLocalArgs);
+   
+// Check if BrowserStack local instance is running
+System.out.println(bsLocal.isRunning());
+  
+// Your test code goes here, from creating the driver instance till the end, i.e. driver.quit
+  
+// Stop the Local instance after your test run is completed, i.e after driver.quit
+bsLocal.stop();
---
+++ take-home-webdriver-test/scr/JavaTest/test_Script2.js
@@ -0,0 +1,8 @@
+// JavaScript test-script code
+// Set these values in your capabilities
+DesiredCapabilities caps = new DesiredCapabilities();
+{
+    ...
+caps.setCapability("browserstack.local", "true");
+    ...
+}
